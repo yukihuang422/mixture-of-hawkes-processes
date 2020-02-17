9501226d9320bf9dd0e4757ed8e462572741361b
yukihuang422 <1427176900@qq.com>
2020-02-17 16:19:42 +0800

    update test

--- MHP_test.py
+++ MHP_test.py
@@ -3,6 +3,7 @@ import numpy as np
 N = 120
 I = 4
 M = 6
+n = 8
 mu_triangle = 0.01
 alpha_triangle = 0.5
 mu = mu_triangle * np.random.rand(M) + 0.5 * mu_triangle
@@ -13,10 +14,17 @@ alpha = alpha_triangle * np.random.rand(M) + 0.5 * alpha_triangle
 # print(alpha[1][1])
 w = 1
 
-P = MHP(mu=mu, alpha=alpha, omega=w, I = I, M = M, N = N)
+P = MHP(mu = mu, alpha = alpha, omega = w, I = I, M = M, N = N)
 P.actor_p()
 P.generate_seq()
 print(P.data)
+data_copy = P.data
+len_data = len(data.copy)
+rand_len = np.random.randint(len_data, size = 8)
+for rand_num in rand_len:
+    data_copy[rand_num][1] = -1
+
+P.EM(Ahat = alpha, mhat = mu, omega = w, seq = data_copy, maxiter=10000)
 
 
 # P.plot_events()
